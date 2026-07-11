49f4b2f33cff796caf593fd04048e946c5b6caa4
leorandal2108 <41757169+leorandal2108@users.noreply.github.com>
2026-07-11 19:06:25 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -195,3 +195,33 @@ searchBox.addEventListener("input",function(){
     renderMenu(filtered);
 
 });
+
+/* ================= GO TO TOP ================= */
+
+const goTopBtn = document.getElementById("goTopBtn");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        goTopBtn.style.display = "flex";
+
+    } else {
+
+        goTopBtn.style.display = "none";
+
+    }
+
+});
+
+goTopBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
